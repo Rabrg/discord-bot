ab78351e76793057d2158637492845925a55009f
Ryan Greene <rabrg96@gmail.com>
2016-09-10 10:44:48 -0500

    Overwatch API command

---
+++ src/me/rabrg/rabrgbot/listener/command/impl/OverwatchProfileCommand.java
@@ -0,0 +1,30 @@
+package me.rabrg.rabrgbot.listener.command.impl;
+
+import me.rabrg.rabrgbot.RabrgBot;
+import me.rabrg.rabrgbot.listener.command.Command;
+import me.rabrg.rabrgbot.overwatch.OverwatchProfileData;
+import net.dv8tion.jda.events.message.MessageReceivedEvent;
+
+public final class OverwatchProfileCommand implements Command {
+
+    @Override
+    public String getName() {
+        return "owprofile";
+    }
+
+    @Override
+    public String getDescription() {
+        return "Gets the profile of the specified Overwatch user";
+    }
+
+    @Override
+    public void run(final RabrgBot bot, final MessageReceivedEvent event, final String args) {
+        if (args == null || args.length() == 0) {
+            bot.sendMessage(event.getChannel(), "You need to specify the tag");
+            return;
+        }
+
+        final OverwatchProfileData data = bot.getOverwatchApi().getProfileData(args);
+        bot.sendMessage(event.getChannel(), "```diff\n" + data.toString() + "\n```");
+    }
+}
